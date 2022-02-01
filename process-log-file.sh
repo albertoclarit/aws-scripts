ca99a1fd9c243f14b509990c130803e3a8802211
Albert Oclarit <albertoclarit@gmail.com>
2022-02-01 16:22:48 +0800

    showing in log line with json on it

--- process-log-file.sh
+++ process-log-file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ($day, $month, $year) = (localtime)[3,4,5];
-$filename="20220131.log"; #sprintf("%04d%02d%02d", $year+1900, $month+1, $day) . ".log";
+$filename=sprintf("%04d%02d%02d", $year+1900, $month+1, $day) . ".log";
 
 open my $info, $filename or die "Could not open $filename: $!";
 
